9b16e9f82d2afbca74d80ecb590a34082bd0d5ad
nshaik0120 <nshaik0120@gmail.com>
2018-12-15 13:03:11 +0530

    login fix

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,7 +8,7 @@ class Login extends Component{
         return(
             <div>
                 Name:<input type="text"/>
-                Password:<input type="password"/>
+                Password:<input type="password"/><br/>
                 <button>Submit</button>
 
             </div>
